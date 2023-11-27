3aab31eac76e7af0deb0ad46987c2f9965bb9ea5
shayan1234554321 <kshayan091@gmail.com>
2023-11-27 15:19:49 +0500

    blog schema

---
+++ models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+    image: String,
+    about: String,
+    title: String,
+    description: String,
+    Content: [String],
+    seo: {
+        title: String,
+        description: String,
+        slug: String
+    }
+
+});
+
+const blogsModel = mongoose.model("blogs", blogSchema);
+module.exports = blogsModel;
